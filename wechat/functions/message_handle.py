c6b26ef75660ca41b0c736a191e2891c9e2d78b2
mrping <https://gitee.com/mrping>
2018-08-04 13:53:55 +0800

    add response_img

--- wechat/functions/message_handle.py
+++ wechat/functions/message_handle.py
@@ -8,11 +8,17 @@ def get_request():
     xml_rec = ET.fromstring(rec)
     to_user = xml_rec.find('ToUserName').text
     from_user = xml_rec.find('FromUserName').text
-    content = xml_rec.find('Content').text
     msg_type = xml_rec.find('MsgType').text
-
+    data = tuple()
+    if msg_type == 'text':
+        content = xml_rec.find('Content').text
+        data = (to_user, from_user, content)
+    elif msg_type == 'image':
+        pic_url = xml_rec.find('PicUrl').text
+        media_id = xml_rec.find('MediaId').text
+        data = (to_user, from_user, pic_url, media_id)
     print(msg_type)
-    return to_user, from_user, content
+    return data
 
 
 def response_text(to_user='', from_user='', msg=''):
@@ -30,6 +36,17 @@ def response_text(to_user='', from_user='', msg=''):
     return response
 
 
-# def response_img(to_user='', from_user='', )
-#     xml_rep = '''
-#             '''
+def response_img(to_user='',from_user='',media_id=''):
+    xml_rep = '''<xml>
+                     <ToUserName><![CDATA[%s]]></ToUserName>
+                     <FromUserName><![CDATA[%s]]></FromUserName>
+                     <CreateTime>%s</CreateTime>
+                     <MsgType><![CDATA[image]]></MsgType>
+                     <Image>
+                     <MediaId><![CDATA[%s]]></MediaId>
+                     </Image>
+                 </xml>'''
+
+    response = make_response(xml_rep % (from_user, to_user, str(int(time.time())), media_id))
+    response.content = 'application/xml'
+    return response
